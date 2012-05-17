e55ee598d6e9aa2591aa4acbdc5296ab08773652
Pete Somerville <pete.somerville@kraya.co.uk>
2012-05-17 17:52:13 +0100

    time logger in python/mysql

---
+++ db_setup.sql
@@ -0,0 +1,26 @@
+/*
+ Run as root, creates user and grants privileges as stored in config.php
+ Why can't use function NOW() (or indeed any function!) as DEFAULT
+ value for start? sure it is in the standards...
+*/
+
+CREATE DATABASE logger;
+
+CREATE USER 'logger_u'@'localhost' IDENTIFIED BY 'this is a dummy pass';
+GRANT SELECT, INSERT, UPDATE, DELETE ON 'logger' TO 'logger_u'@'localhost';
+
+CREATE TABLE tasks (
+            tid INT NOT NULL AUTO_INCREMENT,
+            descript VARCHAR(200),
+            redmine_id VARCHAR(5),
+            PRIMARY KEY (tid)
+);
+
+
+CREATE TABLE logs (
+        tid INT NOT NULL,
+        start DATETIME,
+        stop DATETIME,
+        PRIMARY KEY (tid, start)
+);
+
---
+++ time_logger.py
@@ -0,0 +1,216 @@
+import Tkinter as tk
+import MySQLdb as mdb
+import datetime
+import sys
+
+class ScrolledList(tk.Frame):
+    def __init__(self, options, parent=None):
+        tk.Frame.__init__(self, parent, bg="black")
+        self.pack(expand=tk.YES, fill=tk.BOTH)                  # make me expandable
+        self.makeWidgets(options)
+    def makeWidgets(self, options):                       # or get(ACTIVE)
+        sbary = tk.Scrollbar(self)
+        sbarx = tk.Scrollbar(self)
+        list = tk.Listbox(self, relief=tk.SUNKEN, width=35, height=12,bg="black",fg="white")
+        sbary.config(command=list.yview)                   # xlink sbar and list
+        sbarx.config(command=list.xview,orient=tk.HORIZONTAL)
+        list.config(yscrollcommand=sbary.set,xscrollcommand=sbarx.set)              # move one moves other
+        sbary.pack(side=tk.RIGHT, fill=tk.Y)            # pack first=clip last
+        sbarx.pack(side=tk.BOTTOM, fill=tk.X)
+        list.pack(side=tk.LEFT, expand=tk.YES, fill=tk.BOTH) 
+        self.listbox = list
+
+
+class Logger(tk.Frame):
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)
+       
+        try:
+            self.con = mdb.connect('localhost', 'logger_u', 'this is a dummy pass', 'logger')
+            self.cur = self.con.cursor()
+        except  mdb.Error, e:
+            print "Failed to connect to database"
+            sys.exit(1)
+        self.grid()
+        self.master.title("Log Time Spent On Tasks")
+        self.create_widgets()
+        self.get_tasks()
+        self.current_task = None
+        self.start = None
+          
+        
+    def create_widgets(self):
+        self.tasks = ScrolledList(self)
+        self.tasks.grid(row=0, column=1)
+        self.tasks.listbox.bind("<Double-1>", self.task_a)
+        self.tasks.listbox.bind("<Return>", self.task_a)
+        self.bframe=tk.Frame(self, bg="black")
+        self.bframe.grid(row=0, column=0, sticky="nsew")
+        
+        self.get_tasks_btn = tk.Button(self.bframe, text="All Tasks", command=self.get_tasks, width=8, bg="black", fg="white")
+        self.get_tasks_btn.grid(row=0, column=0, sticky=tk.N+tk.W)
+        self.get_tasks_btn.bind("<Return>", self.get_tasks_a)
+        
+        self.stop_btn = tk.Button(self.bframe, text="Stop", command=self.stop, state=tk.DISABLED, width=8, bg="black", fg="white")
+        self.stop_btn.grid(row=0, column=2, sticky=tk.N)
+        self.stop_btn.bind("<Return>", self.stop_a)
+        
+        self.start_btn = tk.Button(self.bframe, text="Start", command=self.start, width=8, bg="black", fg="white")
+        self.start_btn.grid(row=0, column=1, sticky=tk.W)
+        self.start_btn.bind("<Return>", self.start_a)
+        
+
+        self.details_btn = tk.Button(self.bframe, text="Task Details", command=self.task, width=8, bg="black", fg="white")
+        self.details_btn.grid(row=1, column=0, sticky=tk.W)
+        self.details_btn.bind("<Return>", self.task_a)
+        
+        self.del_task_btn = tk.Button(self.bframe, text="Remove Task", command=self.remove, width=8, bg="black", fg="white")
+        self.del_task_btn.grid(row=1, column=1, sticky=tk.E)
+        self.del_task_btn.bind("<Return>", self.remove_a)
+        
+        self.quit_btn = tk.Button(self.bframe, text="Quit", command=self.quit, width=8, bg="red", fg="black")
+        self.quit_btn.grid(row=1, column=2)
+        self.quit_btn.bind("<Return>", self.quit_a)
+        
+        self.space = tk.Label(self.bframe, height=4, bg="black", fg="white")
+        self.space.grid(row=2, column=0, columnspan=3, sticky=tk.E+tk.W)
+        
+        self.new_btn = tk.Button(self, text="New Task", command=self.new_task, bg="black", fg="white")
+        self.new_btn.grid(row=4, column=0, sticky=tk.W+tk.E)
+        self.new_btn.bind("<Return>", self.new_task_a)
+        
+        self.task_name = tk.Entry(self, bg="grey")
+        self.task_name.grid(row=3, column=0, sticky=tk.E+tk.N+tk.S+tk.W)
+
+
+    def task_a(self, event):
+            self.task()
+    
+    def task(self):
+        if self.tasks.listbox.curselection():
+            self.space.config(text="")
+            tid = int(self.tasks.listbox.get(self.tasks.listbox.curselection()).split("-")[0])
+            self.cur.execute("select SEC_TO_TIME(SUM(TIME_TO_SEC(stop) - TIME_TO_SEC(start))) from logs where tid='%d'"%tid)
+            row = self.cur.fetchone()
+            if row[0] is not None:
+                duration = (datetime.datetime.min+row[0]).time()
+            else:
+                duration = 0
+            self.cur.execute("SELECT descript FROM tasks WHERE tid='%d'"%tid)
+            task = self.cur.fetchone()[0]
+            self.tasks.listbox.delete(0,tk.END)
+            self.tasks.listbox.insert(0, "%d - %s - %s"%(tid, task, duration))
+            self.space.config(text="Time spent: \n %s"% duration)
+            self.del_task_btn.config(state=tk.DISABLED)
+            self.details_btn.config(state=tk.DISABLED)
+            self.start_btn.config(state=tk.DISABLED)
+            self.get_tasks_btn.config(state=tk.NORMAL)
+        else:
+            self.space.config(text="Select a task first...")
+            
+    def new_task(self):
+        des = self.task_name.get()
+        if des is not "":
+            self.space.config(text="")
+            self.cur.execute("INSERT INTO tasks (descript) VALUES ('%s')"%des)
+            self.get_tasks()
+            self.task_name.delete(0,tk.END)
+        else:
+            self.space.config(text="Enter a task description...")
+            
+    def new_task_a(self, event):
+        self.new_task()
+
+    def remove(self):
+        if self.tasks.listbox.curselection():
+            self.space.config(text="")
+            tid = int(self.tasks.listbox.get(self.tasks.listbox.curselection()).split("-")[0])
+            try:
+                self.cur.execute("DELETE FROM logs WHERE tid='%d'"%tid)
+                self.cur.execute("DELETE FROM tasks WHERE tid='%d'"%tid)
+            except mdb.Error, e:
+                print "Error %d: %s" % (e.args[0],e.args[1])
+                sys.exit(1)
+            self.get_tasks()
+        else:
+            self.space.config(text="Select a task to remove...")
+        
+    def remove_a(self, event):
+        self.remove()
+        
+    def start(self):
+        if self.tasks.listbox.curselection():
+            self.start_config()
+            tid, description = self.tasks.listbox.get(self.tasks.listbox.curselection()).split("-")
+            self.space.config(text="Timing - '%s'"%description)
+            tid = int(tid)
+            self.current_task = tid
+            now = datetime.datetime.now()
+            now = now.isoformat(' ').split(".")[0]
+            self.start = now
+            try:
+                self.cur.execute("INSERT INTO logs (tid, start) VALUES ('%d', '%s')"%(tid,now))
+            except mdb.Error, e:
+                print "fail"
+                sys.exit(1)
+        else:
+            self.space.config(text="Select a task to start timing...")
+                   
+    def stop(self):
+        if self.tasks.listbox.curselection():
+            self.stop_config()
+            self.space.config(text="")
+            tid, description = self.tasks.listbox.get(self.tasks.listbox.curselection()).split("-")
+            tid = int(tid)
+            now = datetime.datetime.now()
+            now = now.isoformat(' ').split(".")[0]
+            self.cur.execute("UPDATE logs SET stop='%s' WHERE tid='%d' AND start='%s'"%(now,tid,self.start))
+     
+    def get_tasks(self):
+        self.tasks.listbox.delete(0,tk.END)
+        self.del_task_btn.config(state=tk.NORMAL)
+        self.details_btn.config(state=tk.NORMAL)
+        self.get_tasks_btn.config(state=tk.DISABLED)
+        self.start_btn.config(state=tk.NORMAL)
+        try:
+            self.cur.execute("SELECT tid, descript FROM tasks ORDER BY tid")
+            rows = self.cur.fetchall()
+            for row in rows:
+                self.tasks.listbox.insert(tk.END, "%d - %s" %(row[0], row[1]))
+        except mdb.Error, e:
+            print "Error %d: %s" % (e.args[0],e.args[1])
+            sys.exit(1)
+
+
+    def get_tasks_a(self,event):
+        self.get_tasks()
+    
+    def stop_a(self, event):
+        self.stop()
+    
+    def start_a(self, event):
+        self.start()
+        
+    def quit_a(self, event):
+        self.quit()
+
+    def stop_config(self):
+        self.start_btn.config(state=tk.NORMAL)
+        self.stop_btn.config(state=tk.DISABLED)
+        self.new_btn.config(state=tk.NORMAL)
+        self.details_btn.configure(state=tk.NORMAL)
+        self.del_task_btn.configure(state=tk.NORMAL)
+        self.quit_btn.config(state=tk.NORMAL)
+
+    def start_config(self):
+        self.start_btn.config(state=tk.DISABLED)
+        self.stop_btn.config(state=tk.NORMAL)
+        self.new_btn.config(state=tk.DISABLED)
+        self.get_tasks_btn.config(state=tk.DISABLED)
+        self.details_btn.configure(state=tk.DISABLED)
+        self.del_task_btn.configure(state=tk.DISABLED)
+        self.quit_btn.config(state=tk.DISABLED)
+
+
+log = Logger()
+log.mainloop()
