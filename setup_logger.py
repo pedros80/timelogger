18341177511506688a4ad4667d09a8868af9d764
Pete Somerville <pete.somerville@kraya.co.uk>
2012-05-21 15:21:07 +0100

    changed license to gpl v3

--- setup_logger.py
+++ setup_logger.py
@@ -1,3 +1,36 @@
+#!/usr/bin/env python
+""" 
+time_logger.py
+
+Store start and stop times for named tasks and calculate the previous time
+spent on them.
+Copyright (C) 2012 Peter Somerville
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+requires - python 2.x, Tkinter and MySQLdb.
+run python setup_logger.py to start
+change username and password in Logger() at bottom of script
+
+"""
+
+__author__ = "Peter Somerville"
+__email__ = "peterwsomerville@gmail.com"
+__version__ = "1.0.0"
+__date__ = "21/5/12"
+
+
 import MySQLdb as mdb
 import sys
 
--- time_logger.py
+++ time_logger.py
@@ -4,12 +4,24 @@ time_logger.py
 
 Store start and stop times for named tasks and calculate the previous time
 spent on them.
+Copyright (C) 2012 Peter Somerville
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 requires - python 2.x, Tkinter and MySQLdb.
 run python setup_logger.py to start
 change username and password in Logger() at bottom of script
-
-See also README.md and LICENSE.txt
 
 """
 
